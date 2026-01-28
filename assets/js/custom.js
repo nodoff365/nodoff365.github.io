eaad6e0ea54c8c070d8e1aa689f2c6366871b212
nodoff365 <nodoff365@gmail.com>
2026-01-28 10:55:00 +0900

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,3 +4,15 @@
     sessionStorage.setItem('mode', 'dark');
   }
 })();
+
+document.addEventListener('DOMContentLoaded', () => {
+  const toc = document.getElementById('toc');
+  if (!toc) return;
+
+  // TOC 내부 스크롤 강제 조정 차단
+  toc.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+      e.stopPropagation();
+    }
+  }, true);
+});
